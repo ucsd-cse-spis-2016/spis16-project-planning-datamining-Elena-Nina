86e14c4b03ee2844bb9b26bf1fe1b6e892b591d9
NinaRothenhaus <nrothenh@ucsd.edu>
2016-08-30 11:41:25 -0700

    Update scraper_1.py

--- scraper_1.py
+++ scraper_1.py
@@ -4,16 +4,49 @@ def parseData(fname):
     for l in urllib.urlopen(fname):
         yield l
 
-print "Reading Data..."
-gen = parseData('http://www.ratemyprofessors.com/search.jsp?queryBy=schoolId&schoolName=University+of+California+San+Diego&schoolID=1079&queryoption=TEACHER')
-data = []
-for i in range(1000):
-    my_line = gen.next()
-    if "class=\"name\"" in my_line: #CHANGE WHATS IN THE QUOTES. IT LOOKS FOR THAT
-        print my_line
-        for i in range(10):
+dataDName = [] #733 professors
+gen = parseData('http://www.ucsd.edu/catalog/front/faculty.html')
+
+dataDDepartment = []
+trash = []
+for i in gen:
+    myLine = gen.next()
+    if "</strong>" in myLine:
+        print myLine
+        for i in range(1) and range(2):
             print gen.next()
     else:
-        data.append(my_line)
+        trash.append(myLine)
+
+
+        
+
+gen = parseData('http://www.ucsd.edu/catalog/front/faculty.html')
+for i in gen:
+    myLine = gen.next()
+    if "<strong>" in myLine:
+        a = myLine.find("<strong>")
+        b = myLine.find("</strong>")
+        m = myLine[a+len("<strong>"):b-1]
+        x = m.split()[:2]
+        dataDName.append(x[0] + x[1])
+        
+
+        
+'''
+dataD = []
+trash = []
+
+
+for i in gen:
+    myLine = gen.next()
+    if "<strong>" in myLine:
+        print myLine
+        for i in range(2):
+            print gen.next()
+    else:
+        trash.append(myLine)
 print "done"
+'''
+
 
