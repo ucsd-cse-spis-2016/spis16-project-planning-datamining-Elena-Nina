cbd8dd6003903a0e7f33b5e6540a005cc8fd64f6
NinaRothenhaus <nrothenh@ucsd.edu>
2016-08-31 15:48:49 -0700

    Update rateMyProf_scraper.py

--- rateMyProf_scraper.py
+++ rateMyProf_scraper.py
@@ -1,4 +1,8 @@
+#make list of lists. list has "name" , "rating" or "name", "department"]
+
 from rateMyProfDATA import dataR
+from profDepartDATA import dataD
+
 
 dataRName = []
 for i in dataR:
@@ -12,10 +16,28 @@ for i in dataR:
 
 matrix = [dataRName,dataRRating]
 
-rateMyProfList = []
+rateMyProfList = [] #1676 professors
 
 for i in range(len(dataRName)):
     rateMyProfList.append([row[i] for row in matrix])
+
+
+dataDName = []
+for i in dataD:
+    m = i['Professor']
+    x = m.split()[:2]
+    dataDName.append(x[0] + x[1])
+
+dataDDepartment = []
+for i in dataD:
+    dataDDepartment.append(i['Department'])
+
+matrix = [dataDName,dataDDepartment]
+
+profDepartmentList = [] #1676 professors
+
+for i in range(len(dataDName)):
+    profDepartmentList.append([row[i] for row in matrix])
 
 
 
