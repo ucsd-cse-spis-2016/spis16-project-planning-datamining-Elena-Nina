4dfe4bd25304dce4493bf2f20b8611a0321d8cfe
eguanuna <eguanuna@ucsd.edu>
2016-08-31 09:08:38 -0700

    Add files via upload

---
+++ prof_depart.py
@@ -0,0 +1,388 @@
+# -*- coding: utf-8 -*-
+[
+  {
+    "Abarbanel": "Abrajano, Marisa A., Assistant Professor,\n        Political Science, Warren",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Abramson, Ian S., Professor, Mathematics,\n        Muir",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Acampora, Anthony, Professor Emeritus, Electrical\n        and Computer Engineering, Revelle",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Ackerman, Farrell, Professor, Linguistics,\n        Marshall",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Adams, Joseph A., Professor, Pharmacology,\n        School of Medicine",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Adida, Claire, Assistant Professor,\n         Political Science",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Adler, Amy, Associate Professor, Visual Arts,\n        Revelle",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Adler, Steven, Professor/Provost, Theatre\n        and Dance/Warren",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Agler",
+    "Henry D. I.": "Jim",
+    "Professor": "Professor",
+    "Physics": "Mathematics",
+    "Revelle": "Muir"
+  },
+  {
+    "Abarbanel": "Agnew, Duncan C., Professor, Scripps Institution\n        of Oceanography, Scripps Institution of Oceanography",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Alac, Morana, Assistant Professor, Communication,\n        Revelle",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Alexander, Amy J., Associate Professor, Visual\n        Arts, Marshall",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Alexander, Nicholas M., Professor Emeritus,\n        Pathology, School of Medicine",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Algaze, Guillermo, Professor, Anthropology,\n        Roosevelt",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Ali, Sayed Nageeb, Assistant Professor, Economics,\n        Sixth",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Allen, Eric, Assistant Professor, Scripps\n        Institution of Oceanography/Biological Sciences—Molecular Biology,\n        Scripps Institution of Oceanography/Muir",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Allison, Henry E., Professor Emeritus, Philosophy,\n        Revelle",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Allison, William S., Professor Emeritus, Chemistry\n        and Biochemistry, Muir",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Almutairi, Adah, Assistant Professor, Skaggs\n        School of Pharmacy and Pharmaceutical Sciences, Skaggs School\n        of Pharmacy and Pharmaceutical Sciences",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Aluwihare, Lihini I., Associate Professor,\n        Scripps Institution of Oceanography, Scripps Institution of Oceanography",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Alvarez, Luis, Associate Professor, History,\n        Sixth",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Alvarez, Robert R., Professor, Ethnic Studies,\n        Roosevelt",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Amir, On, Assistant Professor, Rady School\n        of Management, Rady School of Management",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Anagnostaras, Stephan, Assistant Professor,\n        Psychology, Warren",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Anagnostopoulos, Georgios H., Professor, Philosophy,\n        Warren",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Ancoli-Israel, Sonia, Professor-in-Residence,\n        Psychiatry, School of Medicine",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Anderson, Donald W., Professor Emeritus, Computer\n        Science and Engineering/Mathematics, Muir",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Anderson, Michael, Lecturer (PSOE), Physics,\n        Sixth",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Anderson, Norman H., Professor Emeritus, Psychology,\n        Muir",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Anderson, Patrick W., Assistant Professor,\n         Communication, Sixth",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Anderson, Victor C., Professor Emeritus, Electrical\n        and Computer Engineering/Scripps Institution of Oceanography,\n        Scripps Institution of Oceanography/Muir",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Andreoni, James R., Professor, Economics,\n        Marshall",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Anstis",
+    "Henry D. I.": "Stuart",
+    "Professor": "Professor",
+    "Physics": "Psychology",
+    "Revelle": "Roosevelt"
+  },
+  {
+    "Abarbanel": "Antin, David A., Professor Emeritus, Visual\n        Arts, Muir",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Antin, Eleanor, Professor Emerita, Visual\n        Arts, Muir",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Antonovics, Kate, Lecturer (SOE), Economics,\n        Muir",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Appelbaum",
+    "Henry D. I.": "Mark",
+    "Professor": "Professor",
+    "Physics": "Psychology",
+    "Revelle": "Sixth"
+  },
+  {
+    "Abarbanel": "Arias-Castro, Ery, Assistant Professor, Mathematics,\n        Warren",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Armantrout",
+    "Henry D. I.": "Rae",
+    "Professor": "Professor",
+    "Physics": "Literature",
+    "Revelle": "Warren"
+  },
+  {
+    "Abarbanel": "Armi, Laurence, Professor, Scripps Institution\n        of Oceanography, Scripps Institution of Oceanography",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Armour, Jon Christopher, Lecturer (SOE), Biological\n        Sciences—Neurobiology, Revelle",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Arneson, Richard J., Professor, Philosophy,\n        Marshall",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Arnold, James R., Professor Emeritus, Chemistry\n        and Biochemistry, Revelle",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Aroian, Raffi V., Professor, Biological Sciences—Cell\n        and Developmental Biology, Marshall",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Aron, Adam R., Assistant Professor, Psychology,\n        Marshall",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Arovas",
+    "Henry D. I.": "Daniel P.",
+    "Professor": "Professor",
+    "Physics": "Physics",
+    "Revelle": "Revelle"
+  },
+  {
+    "Abarbanel": "Arrhenius, Gustaf, Professor Emeritus, Scripps\n        Institution of Oceanography, Scripps Institution of Oceanography",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Arvaniti, Amalia, Associate Professor, Linguistics,\n        Warren",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Arya, Guarav, Assistant Professor, Mechanical\n        and Aerospace Engineering, Roosevelt",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Asaro, Robert J., Professor, Structural Engineering,\n        Revelle",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Asbeck, Peter M., Professor, Electrical and\n        Computer Engineering, Marshall",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Atkinson, Richard C., Professor Emeritus/Chancellor\n        Emeritus/UC President Emeritus, Psychology/Cognitive Science,\n        Marshall",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Attiyeh, Richard E., Professor Emeritus, Economics,\n        Revelle",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "August, Terrence W., Assistant Professor,\n        Rady School of Management, Rady School of Management",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  },
+  {
+    "Abarbanel": "Azam, Farooq, Professor, Scripps Institution\n        of Oceanography, Scripps Institution of Oceanography",
+    "Henry D. I.": "",
+    "Professor": "",
+    "Physics": "",
+    "Revelle": ""
+  }
+]
