f623eb8a28d59d71eef18d04b3bf656a921da9f7
NinaRothenhaus <nrothenh@ucsd.edu>
2016-08-31 23:43:00 -0700

    Update commonProf.py

--- commonProf.py
+++ commonProf.py
@@ -1,5 +1,16 @@
 from rateMyProf_scraper import profDepartmentList
 from rateMyProf_scraper import rateMyProfList
+from departments import arts_And_HumanitiesSS
+from departments import bio
+from departments import engineering
+from departments import physical
+from departments import management
+from departments import global_policy
+from departments import medicine
+from departments import scripps
+from departments import pharmacy
+
+#to get this to work, 4 windows must be opened! all from desktop!
 
 #list of common professors
 cmProf = []
@@ -32,6 +43,36 @@ for i in rateMyProfList:
         else:
             no = no + 1
 
+
+#turns department into school
+other = []
+for i in cmProfDepart:
+    i[1] = i[1].split()
+    for n in i[1]:
+        if n in physical:
+            i[1] = "physical"
+        elif n in bio:
+            i[1] = 'bio'
+        elif n in engineering:
+            i[1] = 'engineering'
+        elif n in physical:
+            i[1] = 'physical sciences'
+        elif n in management:
+            i[1] = 'management'
+        elif n in global_policy:
+            i[1] = 'global policy'
+        elif n in medicine:
+            i[1] = 'medicine'
+        elif n in scripps:
+            i[1] = 'scripps'
+        elif n in pharmacy:
+            i[1] = 'pharmacy'
+        elif n in arts_And_HumanitiesSS:
+            i[1] = 'Art/Hum/SS'
+        else:
+            other.append(i[1])
+
+
 ''' if cmProfDepart[i][0] == cmProfRate[i][0]
 then...
 dict[cmProfDepart[i][1]] = [cmProfRate[i][1]]'''
@@ -40,11 +81,9 @@ departmentRate = {}
 no = 0
 for i in cmProfDepart:
     for n in cmProfRate:
-        if n[0] == i[0]:
-            departmentRate[i[1]] = [n[1]]
+        if i[0] == n[0]:
+           departmentRate[i[1]] = [n[1]]
         else:
             no = no + 1
 
-
-dict1 = {1:"one" , 2:"two"}
 
