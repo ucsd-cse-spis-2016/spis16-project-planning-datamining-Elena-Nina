d8e1179b8cb1b1d41d7cfa6e721303c380951a5d
NinaRothenhaus <nrothenh@ucsd.edu>
2016-09-08 18:17:26 -0700

    Update and rename rateMyProf_scraper.py to dataR_D_scraper.py

---
+++ dataR_D_scraper.py
@@ -0,0 +1,47 @@
+#make list of lists. list has "name" , "rating" or "name", "department"]
+
+from rateMyProfDATA import dataR
+from profDepartDATA import dataD
+
+#gets Name from dataR
+dataRName = []
+for i in dataR:
+    m = str(i['Text'])
+    x = m.split()[:2]
+    dataRName.append(x[0] + x[1])
+
+#gets rating from dataR
+dataRRating = []
+for i in dataR:
+    dataRRating.append(i['Rating'])
+
+matrix = [dataRName,dataRRating]
+
+rateMyProfList = [] #1676 professors
+
+for i in range(len(dataRName)):
+    rateMyProfList.append([row[i] for row in matrix])
+
+
+dataDName = []
+for i in dataD:
+    m = i['Professor']
+    x = m.split()[:2]
+    dataDName.append(x[0] + x[1])
+
+dataDDepartment = []
+for i in dataD:
+    dataDDepartment.append(i['Department'])
+
+matrix = [dataDName,dataDDepartment]
+
+profDepartmentList = [] #1676 professors
+
+for i in range(len(dataDName)):
+    profDepartmentList.append([row[i] for row in matrix])
+
+
+#dataR[0] turns into dataRName[0]
+#rateMyProfList
+#profDepartmentList
+
